725d444f41ceaa456dd655751cffdfbf1834149a
luisHdz <joseluishdz02@gmail.com>
2022-11-10 22:42:45 -0600

    se agregaron funciones y imagenes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 let iconos = []
 let selecciones = []
+let intentos = 0
+
+let mostrarIntentos = document.getElementById ('intentos')
 
 generarTablero()
 
@@ -39,6 +42,30 @@ function cargarIconos() {
         '../img/img6.png',
         '../img/img7.png',
         '../img/img8.png',
+        '../img/img9.png',
+        '../img/img10.png',
+        '../img/img11.png',
+        '../img/img12.png',
+        '../img/img13.png',
+        '../img/img14.png',
+        '../img/img15.png',
+        '../img/img16.png',
+        '../img/img17.png',
+        '../img/img18.png',
+        '../img/img19.png',
+        '../img/img20.png',
+        '../img/img21.png',
+        '../img/img22.png',
+        '../img/img23.png',
+        '../img/img24.png',
+        '../img/img25.png',
+        '../img/img26.png',
+        '../img/img27.png',
+        '../img/img28.png',
+        '../img/img29.png',
+        '../img/img30.png',
+        '../img/img31.png',
+        '../img/img32.png',
     ]
 }
 
@@ -51,6 +78,8 @@ function seleccionarTarjeta(i) {
     if (selecciones.length == 2) {
         deseleccionar(selecciones)
         selecciones = []
+        intentos++
+        mostrarIntentos.innerHTML = `Intentos: ${intentos}`
     }
 }
 
@@ -61,12 +90,18 @@ function deseleccionar(selecciones) {
     let tarjeta2 = document.getElementById("tarjeta" + selecciones[1])
 
     if (trasera1.innerHTML != trasera2.innerHTML) {
+        trasera1.style.setProperty('box-shadow', '0px 0px 5px 19px rgba(255,0,0,0.75) inset', 'important');
+        trasera2.style.setProperty('box-shadow', '0px 0px 5px 19px rgba(255,0,0,0.75) inset', 'important');
         setTimeout(() => {
             tarjeta1.style.transform = "rotateY(0deg)"
             tarjeta2.style.transform = "rotateY(0deg)"
+            trasera1.style.setProperty('box-shadow', '0px 0px 5px 19px rgb(255, 236, 62) inset', 'important');
+            trasera2.style.setProperty('box-shadow', '0px 0px 5px 19px rgb(255, 236, 62) inset', 'important');
         }, 2000);
     }else{
         setTimeout(() => {
+            trasera1.style.setProperty('box-shadow', '0px 0px 5px 19px rgba(0,255,43,0.75) inset', 'important');
+            trasera2.style.setProperty('box-shadow', '0px 0px 5px 19px rgba(0,255,43,0.75) inset', 'important');
         }, 1000);        
     }
 }
